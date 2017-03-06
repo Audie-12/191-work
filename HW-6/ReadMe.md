e1c71dcfb1cb8c3bf578eac8d2551de0a77862fc
Audra Searcy <searcyaudra@gmail.com>
2017-03-06 06:27:04 -0700

    homework 6

--- HW-6/ReadMe.md
+++ HW-6/ReadMe.md
@@ -1,1 +1,8 @@
+#Problems
+I couldn't figure out how to put the second "if" statement into the first "if" statement with the "for" task.
 
+##Overall
+I found this to be a pretty complex but fun homework assignment. I am going to try and figure out how to solve my problems still and work my way up from there.
+
+##Assignment
+[Project]()
--- HW-6/sketch.js
+++ HW-6/sketch.js
@@ -1,21 +1,59 @@
 function setup() {
-  createCanvas(600,600);
+  createCanvas(600, 400);
+
 }
-
-var r = 0;
-var b = 255;
-
 
 
 function draw() {
-
-
-  r = map(mouseX, 0, 600, 0, 255);
-  b = map(mouseX, 0, 600, 255, 0);
   background(0);
-  fill(r, 0, b);
+  strokeWeight(2);
   stroke(255);
-  ellipse(mouseX, 200, 64, 64);
+
+
+// makes all of the cirlces when you click the mouse
+  if (mouseIsPressed) {
+   for (var x = 0; x <= width; x += 100) {
+     for (var y = 0; y <= height; y += 100) {
+       fill(50, 70, 90);
+       ellipse(x, y, random(0, 25), random(0, 25));
+     }
+   }
+ }
+
+ // Was suppose to create a roll over on the box in the middle but couldnt figure it out
+ if (mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250) {
+   fill(255, 0, 200);
+ }
+ noFill();
+ rectMode(CENTER);
+ rect(300, 200, 100, 100);
+
+// Adds a diagnol line to intersect the circles
+  if (mouseX > 600) {
+    line(100, 0, 0, 100);
+  } if (mouseX > 550) {
+    line(200, 0, 0, 200);
+  } if (mouseX > 500) {
+    line(300, 0, 0, 300);
+  } if (mouseX > 450) {
+    line(400, 0, 0, 400);
+  } if (mouseX > 400) {
+    line(500, 0, 0, 500);
+  } if (mouseX > 350) {
+    line(600, 0, 0, 600);
+  } if (mouseX > 300) {
+    line(700, 0, 0, 700);
+  } if (mouseX > 250) {
+    line(800, 0, 0, 800);
+  } if (mouseX > 200) {
+    line(900, 0, 0, 900);
+  } if (mouseX > 150) {
+    line(1000, 0, 0, 1000);
+  } if (mouseX > 100) {
+    line(1100, 0, 0, 1100);
+  } if (mouseX > 50) {
+    line(1200, 0, 0, 1200);
+  }
+
 
 }
-?
