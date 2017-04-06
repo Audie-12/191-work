29ab66c04554e12e008effa0ce742ca9d43d8184
Audra Searcy <searcyaudra@gmail.com>
2017-04-05 19:26:09 -0600

    edits

--- HW-7/ReadMe.md
+++ HW-7/ReadMe.md
@@ -1,1 +1,3 @@
+# Unsure
 
+This week I had a hard time trying to figure out how to remodel one of my codes because whenever I tried one thing it didn't seem to work properly so in the end I had to do this for mine instead. Which is still nice because I was able to use a function to put everything into one spot and then I could use just that name wherever in the code I chose to put it without have all of that writing in it as well. That way if I wanted to I could then develop a way to add more of them with just using the one word instead of re writing the entire code again.
--- HW-7/sketch.js
+++ HW-7/sketch.js
@@ -1,21 +1,112 @@
 function setup() {
-  createCanvas(600,600);
+  createCanvas(500, 500);
+  background(170, 183, 184);
+
 }
-
-var r = 0;
-var b = 255;
-
-
 
 function draw() {
-
-
-  r = map(mouseX, 0, 600, 0, 255);
-  b = map(mouseX, 0, 600, 255, 0);
-  background(0);
-  fill(r, 0, b);
-  stroke(255);
-  ellipse(mouseX, 200, 64, 64);
-
+  me();
 }
-?
+
+
+
+function me() {
+    // Head
+    fill(250, 215, 160);
+    ellipse(200, 100, 100, 120);
+
+
+    // Eyes
+    push();
+    fill(0);
+    ellipse(225, 100, 10, 10);
+    ellipse(175, 100, 10, 10);
+    pop();
+
+    // Pupils
+    push();
+    fill(255);
+    ellipse(226, 99, 5, 5);
+    ellipse(176, 99, 5, 5);
+    pop();
+
+    // Mouth
+    push();
+    fill(0);
+    rect(190, 135, 20, 5);
+    pop();
+
+    // nose
+    push();
+    triangle(205, 110, 200, 120, 185, 110);
+    pop();
+
+    // Left leg
+    fill(0);
+    push();
+    rect(180, 330, 8, 80);
+    pop();
+
+    // right leg
+    push();
+    rect(210, 330, 8, 80);
+    pop();
+
+    // left foot
+    push();
+    ellipse(173, 410, 30, 15);
+    pop();
+
+    // right foot
+    push();
+    ellipse(226, 410, 30, 15);
+    pop();
+
+    // left arm
+    fill(0);
+    push();
+    rect(125, 205, 35, 10);
+    pop();
+
+    // left arm extended
+    push();
+    rect(125, 205, 10, 55);
+    pop();
+
+    // right arm
+    push();
+    rect(237, 205, 35, 10);
+    pop();
+
+    // right arm extended
+    push();
+    rect(265, 160, 10, 55);
+    pop();
+
+    // left hand
+    push();
+    ellipse(130, 260, 25, 20);
+    pop();
+
+    // right hand
+    push();
+    ellipse(270, 163, 25, 20);
+    pop();
+
+    // body
+    fill(250, 215, 160);
+    push();
+    ellipse(200, 250, 88, 180);
+    pop();
+
+    // scarf
+    fill(0);
+    push();
+    rect(174, 150, 50, 24);
+    pop();
+
+    // scarf bottom
+    push();
+    rect(178, 150, 20, 50);
+    pop();
+  }
